f37e3b3d39969b913cec9c6860a007c6cb46936e
paumonts04 <paumonts04@gmail.com>
2026-06-03 00:20:37 +0200

    Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next'
 import { Bebas_Neue, Space_Mono } from 'next/font/google'
+import { Analytics } from "@vercel/analytics/next"
 import './globals.css'
 
 const bebasNeue = Bebas_Neue({
@@ -29,7 +30,9 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="es" className={`${bebasNeue.variable} ${spaceMono.variable}`}>
-      <body>{children}</body>
+      <body>{children}
+        <Analytics/>
+      </body>
     </html>
   )
 }
